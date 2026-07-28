391582be5d605d759eedb72af9fa0151b6a18cbc
Bear <bear@nuwainfo.com>
2026-07-28 08:20:35 +0800

    4.0.2 release

--- dist/install.ps1
+++ dist/install.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 $repoOwner = "nuwainfo"
 $repoName  = "ffl"
 $app       = "ffl"
-$releaseTag = "v4.0.1"  # Default release version
+$releaseTag = "v4.0.2"  # Default release version
 
 # Environment variables (overridable)
 # FFL_VERSION: Version to install (e.g., v3.7.6)
